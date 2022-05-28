e84f2b1dc67b7e07e9c1cdcc6d45a3efe155fa15
jingdianli <leejingdian@outlook.com>
2022-05-28 08:13:28 +0800

    构建过程: 实现创建订单过程中处理物流信息操作

--- duolaimall-order/order-service/src/main/java/com/cskaoyan/order/biz/handler/LogisticalHandler.java
+++ duolaimall-order/order-service/src/main/java/com/cskaoyan/order/biz/handler/LogisticalHandler.java
@@ -30,6 +30,20 @@ public class LogisticalHandler extends AbstractTransHandler {
 
     @Override
     public boolean handle(TransHandlerContext context) {
+        log.info("begin LogisticalHandler :context:"+context);
+        try {
+            CreateOrderContext createOrderContext=(CreateOrderContext)context;
+            OrderShipping orderShipping = new OrderShipping();
+            orderShipping.setOrderId(String.valueOf(createOrderContext.getOrderId()));
+            orderShipping.setReceiverName(createOrderContext.getUserName());
+            orderShipping.setReceiverAddress(createOrderContext.getStreetName());
+            orderShipping.setReceiverPhone(createOrderContext.getTel());
+            orderShipping.setCreated(new Date());
+            orderShipping.setUpdated(new Date());
+            orderShippingMapper.insert(orderShipping);
+        }catch (Exception e){
+            throw new BizException(OrderRetCode.SHIPPING_DB_SAVED_FAILED.getCode(),OrderRetCode.SHIPPING_DB_SAVED_FAILED.getMessage());
+        }
         return true;
     }
 }
--- duolaimall-order/order-service/src/main/java/com/cskaoyan/order/dal/persistence/OrderShippingMapper.java
+++ duolaimall-order/order-service/src/main/java/com/cskaoyan/order/dal/persistence/OrderShippingMapper.java
@@ -2,7 +2,9 @@ package com.cskaoyan.order.dal.persistence;
 
 
 import com.cskaoyan.order.dal.entitys.OrderShipping;
+import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
 
+@Repository
 public interface OrderShippingMapper extends Mapper<OrderShipping> {
 }
